01ee7c1bb02dd391fcfd6cf9e3d89522d52fc064
Julien Ledun <j.ledun@iosystems.Fr>
2016-07-31 01:39:33 +0200

    simplified app.js
    Signed-off-by: Julien Ledun <j.ledun@iosystems.Fr>

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ app.getip()
 	.then( ( ipaddr ) => {
 		return app.publish( "io_getip", ipaddr );
 	})
-        .then( ( msg ) => {
-		console.log( msg );
-	})
 	.catch( ( err ) => {
-		console.log("err");
 		console.log(err);
 	});
